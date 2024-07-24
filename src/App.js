7b110f05d096cc2132822e86fe12528bf4e57d3f
SakthivelM7 <sakthims198@gmail.com>
2024-07-24 14:08:11 +0530

    list_app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import ItemList from './itemlist';
+
 
 function App() {
+  const [fruits, setFruits] = useState(['Mango','orange','banana']);
+  const [newFruits, setNewFruits] = useState('');
+
+  const handleChange = (a) => {
+    setNewFruits(a.target.value);
+  };
+
+  const addItem = () =>{
+      setFruits([...fruits, newFruits]);
+      setNewFruits('');
+  
+};
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
         <h1>Simple React List Items</h1>
       </header>
-      <main>
-        <ItemList />
-      </main>
+      <input 
+        type = 'text'
+        value = {newFruits}
+        onChange = {handleChange}
+        placeholder='Enter a Newfruits..'
+        />
+        <button onClick={addItem}>Add Fruits</button>
+      <div className='list-items'>
+        <ul>
+          {fruits.map((fruit,index) =>(
+            <li key={index}>{fruit}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
